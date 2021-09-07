4c474690663c5ac86764afed462c85da2cf45d7d
Eddie Yan <eddiey@nvidia.com>
2021-09-07 23:44:30 +0000

    just add an arg for ngpu

--- run_bert_check.sh
+++ run_bert_check.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python -m torch.distributed.launch --nnodes 1 --node_rank 0 --nproc_per_node 2 --master_addr localhost --master_port 6066 bert_check.py --micro-batch-size 16 --num-layers 12 --hidden-size 768 --num-attention-heads 12 --max-position-embeddings 128 --encoder-seq-length 128 --use-cpu-initialization --DDP-impl torch --tensor-model-parallel-size 2
+NGPU=${1:-1}
+python -m torch.distributed.launch --nnodes 1 --node_rank 0 --nproc_per_node NGPU --master_addr localhost --master_port 6066 bert_check.py --micro-batch-size 16 --num-layers 12 --hidden-size 768 --num-attention-heads 12 --max-position-embeddings 128 --encoder-seq-length 128 --use-cpu-initialization --DDP-impl torch --tensor-model-parallel-size NGPU
--- run_bert_check_single_worker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m torch.distributed.launch --nnodes 1 --node_rank 0 --nproc_per_node 1 --master_addr localhost --master_port 6066 bert_check.py --micro-batch-size 16 --num-layers 12 --hidden-size 768 --num-attention-heads 12 --max-position-embeddings 128 --encoder-seq-length 128 --use-cpu-initialization --DDP-impl torch --tensor-model-parallel-size 1
