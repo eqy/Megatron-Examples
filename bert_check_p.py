d0809ab30e1bfb3e462c128513582d043066fef5
Eddie Yan <eddiey@nvidia.com>
2021-09-20 05:00:50 +0000

    switch to pipeline with warmup

--- bert_check_p.py
+++ bert_check_p.py
@@ -29,12 +29,14 @@ from megatron.mpu.utils import VocabUtility
 VOCAB_SIZE = 128
 SEQUENCE_LEN = 128
 MASK_PROB = 0.1
-BATCH_SIZE = 4096
-EASY_MODE = False 
+BATCH_SIZE = 2048
+EASY_MODE = False
+EASY_MODE_SIZ = 32
 STEPS = 5000000
-PRINT_INTERVAL = 2
-
-torch.manual_seed(42)
+PRINT_INTERVAL = 33
+MANUAL_SEED = 42
+DEBUG_PRINT = False
+NO_RUNAHEAD = False
 
 # download a public domain book as corpus
 def download_fancy_data():
@@ -50,7 +52,8 @@ fancy_data = download_fancy_data()
 print(fancy_data.size(0))
 effective_length = fancy_data.size(0) // SEQUENCE_LEN
 effective_length = fancy_data.size(0) - SEQUENCE_LEN
-inds = torch.randperm(effective_length)
+inds = None
+masks = None
 data_idx = 0
 
 def printtensor(tensor):
@@ -71,22 +74,32 @@ def post_processing(lm_output, labels):
 def generate_fancy_data_labels(sequence_len, batch_size):
   global data_idx
   global inds
+  global masks
+  global MANUAL_SEED
   temps = list()
   for i in range(batch_size):
-     #offset = torch.randint(fancy_data.size(0) - SEQUENCE_LEN, (1,))[0]
-     if data_idx >= len(inds):
+     if inds is None or data_idx >= len(inds):
+       # hack as use of RNG will fall out of sync due to pipelines being different
+       torch.manual_seed(MANUAL_SEED)
+       inds = torch.randperm(effective_length, device='cuda')
+       masks = (torch.rand(len(inds)//BATCH_SIZE + 1, BATCH_SIZE, SEQUENCE_LEN, device='cuda') >= MASK_PROB).long()
+       MANUAL_SEED += 1
+
        print("new epoch", len(inds))
        data_idx = 0
-       inds = torch.randperm(effective_length)
+       print("my start", inds[0:5])
+       print("masks_checksum:", torch.sum(masks))
      if EASY_MODE:
-       data_idx = data_idx % 64
-     offset = inds[data_idx] #* SEQUENCE_LEN
+       data_idx_ = data_idx % EASY_MODE_SIZ
+     else:
+       data_idx_ = data_idx
+     offset = inds[data_idx_] #* SEQUENCE_LEN
      data_idx += 1
       
      curr = fancy_data[offset:offset+SEQUENCE_LEN].detach().clone()
      temps.append(curr)
-  temp = torch.stack(temps, dim=0)
-  mask = ( torch.rand(batch_size, sequence_len) >= MASK_PROB ).long()
+  temp = torch.stack(temps, dim=0).cuda()
+  mask = masks[data_idx//BATCH_SIZE]
   mask_not = torch.logical_not(mask)
   data = mask * temp + mask_not*124
   label = temp
@@ -111,8 +124,6 @@ if not isinstance(model, list):
     bert = [model]
 else:
     bert = model
-
-# print(bert)
 
 bert[0] = bert[0].cuda()
 fp16 = bert[0].language_model.encoder._get_layer(0).self_attention.fp16
@@ -147,7 +158,7 @@ optimizer = get_megatron_optimizer(unwrapped_model)
 # check that this is an FP16 optimizer when args.fp16 is set...
 print(type(optimizer))
 
-max_lr=3e-4
+max_lr=3e-3
 min_lr=3e-10
 warmup_steps=1000
 decay_steps=1000000
@@ -186,19 +197,24 @@ start_time = time.time()
 for i in range(STEPS//BATCH_SIZE):
     for partition in bert:
         partition.zero_grad_buffer()
+    optimizer.zero_grad()
 
     input_tensors = []
     output_tensors = []
     losses_reduced = []
 
     num_microbatches = BATCH_SIZE//args.micro_batch_size
-    # second pipeline stage gets input here, first gets input from data
-    input_tensor = p2p_communication.recv_forward()
+    # protocol debugging trackers
+    send_forward_count = 0
+    recv_forward_count = 0
+    send_backward_count = 0
+    recv_backward_count= 0
 
     # rng can be dangerous
     if args.fp16:
       batch_data, batch_label, batch_loss_mask = generate_toy_data_labels(SEQUENCE_LEN, BATCH_SIZE)
     else:
+      # print("GENERATING", i)
       batch_data, batch_label, batch_loss_mask = generate_fancy_data_labels(SEQUENCE_LEN, BATCH_SIZE)
 
     batch_data = batch_data.reshape(batch_data.shape[0]//args.micro_batch_size,
@@ -210,25 +226,68 @@ for i in range(STEPS//BATCH_SIZE):
     batch_loss_mask = batch_loss_mask.reshape(batch_loss_mask.shape[0]//args.micro_batch_size,
                                               args.micro_batch_size,
                                               batch_loss_mask.shape[1])
-    
-    for ub in range(num_microbatches):
-        last_iteration = (ub == (num_microbatches - 1))
 
-        data = batch_data[ub]
-        label = batch_label[ub]
-        loss_mask = batch_loss_mask[ub]
+    num_runahead_microbatches = mpu.get_pipeline_model_parallel_world_size() - mpu.get_pipeline_model_parallel_rank() - 1
+    num_runahead_microbatches = min(num_runahead_microbatches, num_microbatches)
+    if NO_RUNAHEAD:
+        num_runahead_microbatches = 0
 
-        label = label.cuda()
-        data = data.cuda()
-        loss_mask= loss_mask.cuda()
+    if i % PRINT_INTERVAL == 0 and DEBUG_PRINT:
+      print(i, "num runahead:", num_runahead_microbatches, "num microbatches:", num_microbatches)
+      print(i, "data checksum:", torch.sum(batch_data), "label checksum:", torch.sum(batch_label), "mask_checksum:", torch.sum(batch_loss_mask))
+
+    data_ub_idx = 0
+
+    # runahead to reduce pipeline stalls
+    for ub_runahead_warmup in range(num_runahead_microbatches):
+        input_tensor = p2p_communication.recv_forward()
+        recv_forward_count += 1
+        data = batch_data[data_ub_idx]
+        label = batch_label[data_ub_idx]
+        loss_mask = batch_loss_mask[data_ub_idx]
+        data_ub_idx += 1
+
         # no padding in either toy or "fancy" data
         padding_mask = torch.ones_like(data, device='cuda')
 
         # start of "forward_step function"
         unwrapped_model = unwrap_model(bert[0], (torchDDP, LocalDDP, Float16Module))
         unwrapped_model.set_input_tensor(input_tensor)
-
         output_tensor = bert[0](data, padding_mask, tokentype_ids=None, lm_labels=None)
+        if DEBUG_PRINT and mpu.is_pipeline_first_stage() and i % PRINT_INTERVAL == 0:
+            print("first stage did inference on data:")
+            printtensor(data[:1,:])
+        assert not mpu.is_pipeline_last_stage()
+        # end of "forward_step function"
+        p2p_communication.send_forward(output_tensor)
+        send_forward_count += 1
+        input_tensors.append(input_tensor)
+        output_tensors.append(output_tensor)
+
+    if num_runahead_microbatches < num_microbatches:
+        input_tensor = p2p_communication.recv_forward()
+        recv_forward_count += 1
+     
+    for ub in range(num_runahead_microbatches, num_microbatches):
+        #if DEBUG_PRINT and i % PRINT_INTERVAL == 0:
+        #    print("rank", mpu.get_pipeline_model_parallel_rank(), ub)
+        last_iteration = (ub == (num_microbatches - 1))
+
+        data = batch_data[data_ub_idx]
+        label = batch_label[data_ub_idx]
+        loss_mask = batch_loss_mask[data_ub_idx]
+        data_ub_idx += 1
+
+        # no padding in either toy or "fancy" data
+        padding_mask = torch.ones_like(data, device='cuda')
+
+        # start of "forward_step function"
+        unwrapped_model = unwrap_model(bert[0], (torchDDP, LocalDDP, Float16Module))
+        unwrapped_model.set_input_tensor(input_tensor)
+        output_tensor = bert[0](data, padding_mask, tokentype_ids=None, lm_labels=None)
+        #if DEBUG_PRINT and mpu.is_pipeline_first_stage() and i % PRINT_INTERVAL == 0:
+        #    print("first stage did inference on data:")
+        #    printtensor(data[:1,:])
         if mpu.is_pipeline_last_stage():
             output_tensor, _ = output_tensor
             output_tensor, orig_output = post_processing(output_tensor, label)
@@ -239,12 +298,19 @@ for i in range(STEPS//BATCH_SIZE):
                 lm_loss_.view(-1) * loss_mask.reshape(-1)) / loss_mask.sum()
             prescale_loss = lm_loss / num_microbatches
             output_tensor = prescale_loss
+            #if DEBUG_PRINT and i % PRINT_INTERVAL == 0:
+            #    print("last stage did loss on label:", f"({data_ub_idx})")
+            #    printtensor(label[:1,:])
         # end of "forward_step function"
         output_tensor_grad = p2p_communication.send_forward_recv_backward(output_tensor)
+        send_forward_count += 1
+        recv_backward_count += 1
 
-        # no warmup, so we don't need this?
-        # input_tensors.append(input_tensor)
-        # output_tensors.append(output_tensor)
+        input_tensors.append(input_tensor)
+        output_tensors.append(output_tensor)
+
+        input_tensor = input_tensors.pop(0)
+        output_tensor = output_tensors.pop(0)
         
         # start of "backward_step function"
         if input_tensor is not None:
@@ -255,18 +321,65 @@ for i in range(STEPS//BATCH_SIZE):
         input_tensor_grad = None
         if input_tensor is not None:
             input_tensor_grad = input_tensor.grad
+        # end of "backward_step function"
+        #if DEBUG_PRINT and i % PRINT_INTERVAL == 0:
+        #    if input_tensor_grad is not None:
+        #        print("steady state grad sum:", torch.sum(input_tensor_grad))
+        #    else:
+        #        print("none in steady-state")
 
         if last_iteration:
+            input_tensor = None
             p2p_communication.send_backward(input_tensor_grad)
+            send_backward_count += 1
         else:
             input_tensor = p2p_communication.send_backward_recv_forward(input_tensor_grad)
+            send_backward_count += 1
+            recv_forward_count += 1
+
+    for ub_runahead_cooldown in range(num_runahead_microbatches):
+        output_tensor_grad = p2p_communication.recv_backward()
+        recv_backward_count += 1
+        input_tensor = input_tensors.pop(0)
+        output_tensor = output_tensors.pop(0)
+
+        # start of "backward_step function"
+        if input_tensor is not None:
+            input_tensor.retain_grad()
+        if output_tensor_grad is None:
+            output_tensor = optimizer.scale_loss(output_tensor)
+        torch.autograd.backward(output_tensor, grad_tensors=output_tensor_grad)
+        input_tensor_grad = None
+        if input_tensor is not None:
+            input_tensor_grad = input_tensor.grad
+        # end of "backward_step function"
+        #if DEBUG_PRINT and i % PRINT_INTERVAL == 0:
+        #    if input_tensor_grad is not None:
+        #        print("tail grad sum:", torch.sum(input_tensor_grad))
+        #    else:
+        #        print("none in tail")
+        p2p_communication.send_backward(input_tensor_grad)
+        send_backward_count += 1
+
+    assert len(input_tensors) == 0
+    assert len(output_tensors) == 0
+    # assert data_ub_idx == num_microbatches
 
     if args.DDP_impl == 'local':
         bert[0].allreduce_gradients()
 
+    if (mpu.is_pipeline_first_stage(ignore_virtual=True) or
+        mpu.is_pipeline_last_stage(ignore_virtual=False)) and \
+            mpu.get_pipeline_model_parallel_world_size() > 1:
+        assert bert[0] == bert[-1]
+        unwrapped_model = unwrap_model(bert[0], (torchDDP, LocalDDP, Float16Module))
+        if unwrapped_model.share_word_embeddings:
+            word_embeddings_weight = unwrapped_model.word_embeddings_weight()
+            grad = word_embeddings_weight.main_grad 
+            torch.distributed.all_reduce(grad, group=mpu.get_embedding_group())
+
     # fp16_lm_cross_entropy False by default, so we add a cast to float here
     # used if we turn off postprocessing in the bert model
-    # lm_loss_ = mpu.vocab_parallel_cross_entropy(output_tensor.float(), label)
     update_successful, grad_norm, num_zeros_in_grad = optimizer.step()
     if update_successful:
         lr_scheduler.step(BATCH_SIZE)
@@ -275,11 +388,18 @@ for i in range(STEPS//BATCH_SIZE):
         print("update failed")
 
     if i % PRINT_INTERVAL == 0:
-        print((i+1)*BATCH_SIZE/(time.time() - start_time), "samples/sec")
         if mpu.is_pipeline_last_stage():
             printtensor(data[:2,])
             preds = torch.argmax(orig_output, dim=2) * loss_mask
             cleaned = data * torch.logical_not(loss_mask)
             printtensor((cleaned + preds)[:2,:].long())
-            print("PRESCALE LOSS:", prescale_loss)
-
+            print("LABEL")
+            printtensor(label[:2, :])
+            print((i+1)*BATCH_SIZE, "UNSCALED LOSS:", lm_loss)
+        if mpu.is_pipeline_first_stage():
+            print((i+1)*BATCH_SIZE/(time.time() - start_time), "samples/sec")
+        if DEBUG_PRINT and i % PRINT_INTERVAL == 0:
+            print(mpu.get_pipeline_model_parallel_rank(), {'send_forward_count':send_forward_count,
+                                                           'recv_forward_count':recv_forward_count,
+                                                           'send_backward_count':send_backward_count,
+                                                           'recv_backward_count':recv_backward_count})
--- run_bert_check_p.sh
+++ run_bert_check_p.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NGPU=${1:-1}
 UB_SIZE=${2:-64}
-python -m torch.distributed.launch --nnodes 1 --node_rank 0 --nproc_per_node $NGPU --master_addr localhost --master_port 6066 bert_check_p.py --micro-batch-size $UB_SIZE --num-layers 16 --hidden-size 768 --num-attention-heads 16 --max-position-embeddings 128 --encoder-seq-length 128 --attention-dropout 0.0 --hidden-dropout 0.0 --DDP-impl local --tensor-model-parallel-size 1 --pipeline-model-parallel-size $NGPU $3 $4
+python -m torch.distributed.launch --nnodes 1 --node_rank 0 --nproc_per_node $NGPU --master_addr localhost --master_port 6066 bert_check_p2.py --micro-batch-size $UB_SIZE --num-layers 16 --hidden-size 768 --num-attention-heads 16 --max-position-embeddings 128 --encoder-seq-length 128 --attention-dropout 0.0 --hidden-dropout 0.0 --DDP-impl local --tensor-model-parallel-size 1 --pipeline-model-parallel-size $NGPU $3 $4
